234e5c585f1a010e5e7b017e8a770b006c5b3e8f
Viktor Nagy <edu_mqvc_4680@diakoffice.onmicrosoft.com>
2020-10-12 19:43:45 +0200

    User login and logout added

--- frontend_/src/App.js
+++ frontend_/src/App.js
@@ -6,20 +6,22 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
     return (
-            <Router>
-                <Header/>
-                <main className='py-3'>
-                    <Container>
-                       <Route path='/' component={HomeScreen} exact/>
-                       <Route path='/product/:id' component={ProductScreen}/>
-                       <Route path='/cart/:id?' component={CartScreen}/>
-                    </Container>
-                </main>
-                <Footer/>
-            </Router>
+        <Router>
+            <Header/>
+            <main className='py-3'>
+                <Container>
+                    <Route path='/login' component={LoginScreen}/>
+                    <Route path='/product/:id' component={ProductScreen}/>
+                    <Route path='/cart/:id?' component={CartScreen}/>
+                    <Route path='/' component={HomeScreen} exact/>
+                </Container>
+            </main>
+            <Footer/>
+        </Router>
     );
 }
 
