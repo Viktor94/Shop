4452a5d23d389c5c0079678288aead87147ed458
Viktor Nagy <edu_mqvc_4680@diakoffice.onmicrosoft.com>
2020-10-20 10:24:33 +0200

    Update user profile added

---
+++ frontend_/src/screens/ProfileScreen.js
@@ -0,0 +1,112 @@
+import React, {useEffect, useState} from 'react';
+import {Button, Col, Form, FormControl, FormGroup, FormLabel, Row} from 'react-bootstrap'
+import {useDispatch, useSelector} from "react-redux";
+import {getUserDetails, updateUserProfile} from "../actions/userActions";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+
+const ProfileScreen = ({location, history}) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [message, setMessage] = useState(null);
+
+    const dispatch = useDispatch();
+
+    const userDetails = useSelector((state) => state.userDetails);
+
+    const {loading, error, user} = userDetails;
+
+    const userLogin = useSelector((state) => state.userLogin);
+
+    const {userInfo} = userLogin;
+
+    const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+
+    const {success} = userUpdateProfile;
+
+    useEffect(() => {
+        if (!userInfo) {
+            history.push('/login')
+        } else {
+            if (!user.name) {
+                dispatch(getUserDetails('profile'))
+            } else {
+                setName(user.name)
+                setEmail(user.email)
+            }
+        }
+    }, [dispatch, history, userInfo, user])
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            setMessage('Password do not match')
+        } else {
+            dispatch(updateUserProfile({id: user._id, name, email, password}))
+        }
+    }
+
+    return <Row>
+        <Col md={3}>
+            <h2>User Profile</h2>
+            {message && <Message variant='danger'>{message}</Message>}
+            {error && <Message variant='danger'>{error}</Message>}
+            {success && <Message variant='success'>Profile Updated</Message>}
+            {loading && <Loader/>}
+            <Form onSubmit={submitHandler}>
+                <FormGroup controlId='name'>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl
+                        type='name'
+                        placeholder='Enter name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    >
+                    </FormControl>
+                </FormGroup>
+
+                <FormGroup controlId='email'>
+                    <FormLabel>Email Address</FormLabel>
+                    <FormControl
+                        type='email'
+                        placeholder='Enter email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    >
+                    </FormControl>
+                </FormGroup>
+
+                <FormGroup controlId='password'>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl
+                        type='Password'
+                        placeholder='Enter password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    >
+                    </FormControl>
+                </FormGroup>
+
+                <FormGroup controlId='confirmPassword'>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl
+                        type='Password'
+                        placeholder='Confirm password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    >
+                    </FormControl>
+                </FormGroup>
+
+                <Button type='submit' variant='primary'>Update</Button>
+            </Form>
+        </Col>
+        <Col md={9}>
+            <h2>My Orders</h2>
+        </Col>
+    </Row>;
+};
+
+export default ProfileScreen;
